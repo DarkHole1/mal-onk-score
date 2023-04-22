6c85f44bfb4a4a7b99b694018396e91f15d10f8f
Dark Hole <dark.hole1@yandex.ru>
2023-04-22 19:05:35 +0300

    Changed date format

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,6 @@ import * as fs from 'fs/promises'
     const data = await mal.get_anime_details(52034)
     await fs.appendFile(
         'data.txt',
-        ['"'+new Date().toLocaleString()+'"', data.mean, data.popularity, data.num_list_users, data.num_scoring_users].join(',')+'\n'
+        [new Date().toISOString(), data.mean, data.popularity, data.num_list_users, data.num_scoring_users].join(',')+'\n'
     )
 })()
