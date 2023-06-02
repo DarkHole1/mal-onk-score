2b6224c80f400319918dadbf3ae4468225725e0b
Dark Hole <dark.hole1@yandex.ru>
2023-06-02 11:25:28 +0300

    Add main page

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,11 @@ import path from 'path'
 
 const app = express()
 
-app.get('/oshi-no-ko.csv', async (req, res) => {
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../static/index.html'))
+})
+
+app.get('/oshi-no-ko.csv', (req, res) => {
     res.header('Content-Disposition', 'attachement; filename="oshi-no-ko.csv"')
     res.sendFile(path.join(__dirname, '../data.txt'))
 })
